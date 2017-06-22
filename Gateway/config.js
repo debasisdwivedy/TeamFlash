6a8510c43ef4d26cc77a5d3486fce1d777706c65
Debasis Dwivedy <dwivedy.debasis@gmail.com>
2017-06-22 12:10:32 -0400

    Refactor

--- Gateway/config.js
+++ Gateway/config.js
@@ -4,8 +4,8 @@ module.exports = {
   bucketName: '[your Google Cloud Storage bucket name]',
   cookieSecret: '[cooking signing key]',
   oauth2: {
-    clientId: '842652899246-1eqq9510atpnsu3aktjmup6l8kmqk3q3.apps.googleusercontent.com',
-    clientSecret: 'htkmqlCVmuTf199NEk56kAnt',
+    clientId: '',
+    clientSecret: '',
     redirectUrl: process.env.REDIRECT_URL || 'http://ec2-52-9-64-108.us-west-1.compute.amazonaws.com:8888/oauth2callback'
   }
 };
